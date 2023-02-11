223d5be25cab4f69667b6cd4766d5660064df24d
Sami Sunil Yaswanth <samisunilyaswanth@gmail.com>
2023-02-11 12:33:23 +0530

    files are added

---
+++ GenericsExample1_Feb8/GenericsExample1_Feb8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample1_Feb8
+{
+    class Program
+    {
+        public class Student
+        { 
+            public void print<Integer,String>(Integer param1, String param2)
+            {
+                Console.WriteLine($"Parameter1:{param1}, Parameter2:{param2}");
+            }
+
+            public void printOne<T>(T num1,T num2) 
+            {
+                int sum = Convert.ToInt32(num1) + Convert.ToInt32(num2); 
+                Console.WriteLine($"Num1:{num1}, Num2:{num2}, Sum:{sum}"); 
+                Console.WriteLine(sum);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.print(1, "sunil"); 
+            student.printOne(7, 4); 
+            
+
+        }
+    }
+}
---
+++ Generics_Feb8/Generics_Feb8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Feb8
+{
+    class Program
+    {
+       //class which hold the generic value
+       public class GenericExample
+        {
+            public void add<T>(T param1, T param2)
+            {
+                
+                Console.WriteLine($"Parameter1:{param1}, Parameter2:{param2}"); 
+            }
+        }
+
+            
+        static void Main(string[] args)
+        {
+            GenericExample genericExample= new GenericExample();
+            genericExample.add<int>(1, 2); 
+            genericExample.add("hello", "world");
+        }
+    }
+}
